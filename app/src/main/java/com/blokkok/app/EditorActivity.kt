d0e85519fba1e981cb864e23117bd23eb12725b8
TheClashFruit <clash.fruit123@gmail.com>
2021-07-11 15:31:21 +0200

    feat: add back button to EditorActivity

--- app/src/main/java/com/blokkok/app/EditorActivity.kt
+++ app/src/main/java/com/blokkok/app/EditorActivity.kt
@@ -1,11 +1,20 @@
 package com.blokkok.app
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class EditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
+
+        val actionBar = findViewById<View>(R.id.toolBar) as Toolbar
+
+        setSupportActionBar(actionBar)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
     }
 }
