be4a0b42c03103831d03e5a9ebd55ba05f7067b5
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-12 15:25:33 +0700

    feat: Implement ProjectEditor

---
+++ app/src/main/java/com/blokkok/app/managers/projects/JavaFile.kt
@@ -0,0 +1,6 @@
+package com.blokkok.app.managers.projects
+
+data class JavaFile(
+    val name: String,
+    val packageName: String,
+)
--- app/src/main/java/com/blokkok/app/managers/projects/ProjectEditor.kt
+++ app/src/main/java/com/blokkok/app/managers/projects/ProjectEditor.kt
@@ -6,4 +6,78 @@ class ProjectEditor(
     private val dataDir: File
 ) {
 
+    private val javaFolder = File(dataDir, "java")
+    private val layoutFolder = File(dataDir, "res/layout")
+
+    init {
+        if (!javaFolder.exists()) javaFolder.mkdir()
+        if (!layoutFolder.exists()) layoutFolder.mkdir()
+    }
+
+    fun listJava(folder: File = javaFolder, currentPackage: String = ""): List<JavaFile> =
+        ArrayList<JavaFile>().apply {
+            folder.listFiles()!!.forEach {
+                if (it.isDirectory) {
+                    addAll(listJava(it, "$currentPackage.${it.name}"))
+                } else {
+                    add(JavaFile(it.nameWithoutExtension, currentPackage))
+                }
+            }
+        }
+
+    /**
+     * Returns a list of layout names without it's extension, example: "main"
+     */
+    fun listLayout(): List<String> = layoutFolder.listFiles()!!.map { it.nameWithoutExtension }
+
+    /**
+     * Used to read and write java files
+     * Example(s):
+     * val mainActivityCode = java["com.hello.world.MainActivity"]
+     * java["com.hello.world.MainActivity"] = "// oops, it's gone now"
+     */
+    val java = object {
+        /**
+         * Read the code of a java file, will return null if the given class doesn't exist
+         */
+        fun get(name: String): String? {
+            val path = name.replace(".", "/")
+            val file = File(javaFolder, "$path.java")
+
+            return if (!file.exists()) null else file.readText()
+        }
+
+        /**
+         * Write code to a java file, will create a new file if the given name doesn't exist
+         */
+        fun set(name: String, code: String) {
+            val path = name.replace(".", "/")
+            val file = File(javaFolder, "$path.java")
+
+            if (!file.exists()) file.createNewFile()
+
+            file.writeText(code)
+        }
+    }
+
+    val layout = object {
+        /**
+         * Read the code of a layout xml file, will return null if the given class doesn't exist
+         */
+        fun get(name: String): String? {
+            val file = File(layoutFolder, "$name.xml")
+
+            return if (!file.exists()) null else file.readText()
+        }
+
+        /**
+         * Write code to a layout xml file, will create a new file if the given name doesn't exist
+         */
+        fun set(name: String, code: String) {
+            val file = File(javaFolder, "$name.xml")
+            if (!file.exists()) file.createNewFile()
+
+            file.writeText(code)
+        }
+    }
 }
