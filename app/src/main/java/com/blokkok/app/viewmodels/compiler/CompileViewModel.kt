39787046cdf8946c552feebee3318f256fa35627
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-18 09:14:34 +0700

    feat: Integrate aapt2 to CompileViewModel

--- app/src/main/java/com/blokkok/app/viewmodels/compiler/CompileViewModel.kt
+++ app/src/main/java/com/blokkok/app/viewmodels/compiler/CompileViewModel.kt
@@ -25,21 +25,82 @@ class CompileViewModel : ViewModel() {
 
     fun startCompilation(project: ProjectMetadata, context: Context) {
 
-        viewModelScope.launch(Dispatchers.IO) {
-            val javaFiles = File(context.applicationInfo.dataDir, "projects/${project.id}/data/java/${project.packageName.split(".")[0]}/")
-            val cacheFolder = File(context.applicationInfo.dataDir, "projects/${project.id}/cache/")
+        val dataDir = context.applicationInfo.dataDir
 
-            val classesCacheFolder = File(cacheFolder, "classes")
-            val dexCacheFolder = File(cacheFolder, "dex")
-            val classesDex = File(dexCacheFolder, "classes.jar")
+        val androidJar = File("$dataDir/binaries/android.jar") // android.jar file
+        val packagePath = project.packageName.replace(".", "/") // The path of the package
+        val firstPackage = project.packageName.split(".")[0] // First item in the package
+
+        val javaFiles =
+            File(dataDir, "projects/${project.id}/data/java/$firstPackage/")
+
+        val resFolder =
+            File(dataDir, "projects/${project.id}/data/res")
+
+        val androidManifestXml =
+            File(dataDir, "projects/${project.id}/data/AndroidManifest.xml")
+
+        val cacheFolder =
+            File(dataDir, "projects/${project.id}/cache/")
+
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val classesCacheFolder      = File(cacheFolder, "classes") // where compiled java files from both generated java files and the project's code are located
+            val generatedJavaFolder     = File(cacheFolder, "genJava") // where java generated files like R.java are located
+            val dexCacheFolder          = File(cacheFolder, "dex")     // where the dex-ed class files from the classes folder are located
+            val compiledResCacheFolder  = File(cacheFolder, "compiledRes") // where the compiled resources files (resources.zip) are located
+
+            val resourcesZip = File(compiledResCacheFolder, "resources.zip") // the compiled resources
+            val classesDex = File(dexCacheFolder, "classes.jar") // dex-ed classes from both generated java files and project's code
+            val resOutApk = File(cacheFolder, "res.apk") // output apk with resources
+
+            // mkdirs ==============================================================================
+            cacheFolder.mkdirs()
 
             classesCacheFolder.mkdirs()
+            generatedJavaFolder.mkdirs()
             dexCacheFolder.mkdirs()
+            compiledResCacheFolder.mkdirs()
+            // mkdirs ==============================================================================
 
+            // Picks the compiler and the dexer
             val compiler = CompilerPicker.pickCompiler()
             val dexer = CompilerPicker.pickDexer()
 
-            // Run ecj
+            ///////////////////////
+            // Start compilation //
+            ///////////////////////
+
+            // First, run aapt2 to compile resources
+
+            val aapt2crRetVal = compileResources(
+                resFolder, // the res folder
+                resourcesZip // and the output file will be cache/compiledRes/resources.zip
+            )
+
+            if (aapt2crRetVal != 0) {
+                log("aapt2 returned a non-zero status"); return@launch
+            } else {
+                log("aapt2 has finished compiling resources")
+            }
+
+            // Then, link the resources with aapt2
+
+            val aapt2lrRetVal = linkResources(
+                androidJar, // the android.jar file
+                androidManifestXml, // the AndroidManifest.xml file
+                File(generatedJavaFolder, packagePath), // output folder of R.java
+                resOutApk, // output apk
+                resourcesZip // the compiled resources
+            )
+
+            if (aapt2lrRetVal != 0) {
+                log("aapt2 returned a non-zero status"); return@launch
+            } else {
+                log("aapt2 has finished linking resources")
+            }
+
+            // Then run ecj to compile java sources
 
             log("ECJ is starting to compile")
 
@@ -76,24 +137,43 @@ class CompileViewModel : ViewModel() {
             } else {
                 log("D8 has finished dex-ing")
             }
-
-            log("\nAAPT2 starting to run")
-
-            val aapt2RetVal = NativeBinariesManager
-                .executeCommand(
-                    NativeBinariesManager.NativeBinaries.AAPT2,
-                    arrayOf("link"),
-                    { runBlocking { log("AAPT2 >> $it") } },
-                    { runBlocking { log("AAPT2 ERR >> $it") } }
-                )
-
-            if (aapt2RetVal != 0) {
-                log("aapt2 returned a non-zero status")
-                return@launch
-
-            } else {
-                log("aapt2 has finished running")
-            }
         }
+    }
+
+    private suspend fun compileResources(resFolder: File, resourcesZipOutput: File): Int {
+        log("\nAAPT2 is compiling resources")
+
+        return NativeBinariesManager
+            .executeCommand(
+                NativeBinariesManager.NativeBinaries.AAPT2,
+                arrayOf("compile", "--dir", resFolder.absolutePath, "-o", resourcesZipOutput.absolutePath, "-v"),
+                { runBlocking { log("AAPT2 >> $it") } },
+                { runBlocking { log("AAPT2 ERR >> $it") } }
+            )
+    }
+
+    private suspend fun linkResources(
+        androidJar: File,
+        androidManifestXml: File,
+        rJavaOutput: File,
+        outputApk: File,
+        resourcesZip: File
+    ): Int {
+        log("\nAAPT2 is linking resources")
+
+        return NativeBinariesManager
+            .executeCommand(
+                NativeBinariesManager.NativeBinaries.AAPT2,
+                arrayOf("link",
+                    "-I", androidJar.absolutePath,
+                    "--auto-add-overlay",
+                    "--manifest", androidManifestXml.absolutePath,
+                    "--java", rJavaOutput.absolutePath,
+                    "-o", outputApk.absolutePath,
+                    resourcesZip.absolutePath
+                ),
+                { runBlocking { log("AAPT2 >> $it") } },
+                { runBlocking { log("AAPT2 ERR >> $it") } }
+            )
     }
 }
