19fc389a5625336492208be8a3a6d7888db4fabf
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-18 08:30:16 +0700

    feat: Add sdklib jar as a dependency

--- app/src/main/java/com/blokkok/app/MainActivity.kt
+++ app/src/main/java/com/blokkok/app/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.blokkok.app
 
 import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
