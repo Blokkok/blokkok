14acb9163ee81cbf860fce7e88dcc4ab2545826c
TheClashFruit <clash.fruit123@gmail.com>
2021-07-26 16:39:50 +0200

    feat: StoreItemAdapter

--- app/src/main/java/com/blokkok/app/adapters/StoreItemAdapter.kt
+++ app/src/main/java/com/blokkok/app/adapters/StoreItemAdapter.kt
@@ -1,4 +1,48 @@
 package com.blokkok.app.adapters
 
-class StoreItemAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.blokkok.app.R
+import com.blokkok.app.managers.projects.ProjectMetadata
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class StoreItemMetadata(
+    val title: String,
+    val description: String,
+    val whats_new: String,
+    val downloads: Int,
+    val likes: List<String>,
+    val owner: String,
+    val type: String,
+    val file: String,
+    val publish_date: Int,
+    val update_date: Int,
+    val visibility: String,
+);
+
+class StoreItemAdapter(
+    private var storeList: List<StoreItemMetadata>
+
+) : RecyclerView.Adapter<ProjectsRecyclerView.ViewHolder>() {
+    fun updateView(newStoreList: List<StoreItemMetadata>) {
+        storeList = newStoreList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectsRecyclerView.ViewHolder =
+        ProjectsRecyclerView.ViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.store_item, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: ProjectsRecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/blokkok/app/fragments/main/StoreFragment.kt
+++ app/src/main/java/com/blokkok/app/fragments/main/StoreFragment.kt
@@ -1,11 +1,19 @@
 package com.blokkok.app.fragments.main
 
-import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import com.blokkok.app.R;
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.blokkok.app.R
+import com.blokkok.app.adapters.StoreItemAdapter
+import com.blokkok.app.adapters.StoreItemMetadata
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class StoreFragment : Fragment() {
 
@@ -18,6 +26,38 @@ class StoreFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         val root = inflater.inflate(R.layout.fragment_store, container, false);
+
+        val db = Firebase.firestore;
+        val sharedContent = db.collection("shared");
+
+        val recyclerView1 = root.findViewById<RecyclerView>(R.id.recyclerView1);
+        val recyclerView2 = root.findViewById<RecyclerView>(R.id.recyclerView2);
+        val recyclerView3 = root.findViewById<RecyclerView>(R.id.recyclerView3);
+        val recyclerView4 = root.findViewById<RecyclerView>(R.id.recyclerView4);
+
+        recyclerView1.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView2.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView3.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView4.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+
+        sharedContent.get().addOnSuccessListener { document ->
+            if (document != null) {
+                val StoreItemList: List<StoreItemMetadata> = ArrayList();
+
+                for (snapshot in document) {
+                    StoreItemList.plusElement(snapshot.toObject(StoreItemMetadata::class.java));
+                }
+
+                recyclerView1.adapter = StoreItemAdapter(StoreItemList);
+                recyclerView2.adapter = StoreItemAdapter(StoreItemList);
+                recyclerView3.adapter = StoreItemAdapter(StoreItemList);
+                recyclerView4.adapter = StoreItemAdapter(StoreItemList);
+
+                // TODO: fix java.lang.RuntimeException: Could not deserialize object. Class com.blokkok.app.adapters.StoreItemMetadata does not define a no-argument constructor. If you are using ProGuard, make sure these constructors are not stripped
+            } else {
+                Log.d("DataBase", "No such document");
+            }
+        }.addOnFailureListener { exception -> Log.d("DataBase", "get failed with ", exception); };
 
         return root;
     }
