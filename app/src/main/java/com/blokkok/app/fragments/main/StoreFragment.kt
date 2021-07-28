229ea968d5e9e0fc160907b42b113d93957e91d0
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-28 15:41:19 +0700

    fix: fix store crashing

--- app/src/main/java/com/blokkok/app/fragments/main/StoreFragment.kt
+++ app/src/main/java/com/blokkok/app/fragments/main/StoreFragment.kt
@@ -12,10 +12,8 @@ import com.blokkok.app.R
 import com.blokkok.app.adapters.StoreItemAdapter
 import com.blokkok.app.adapters.StoreItemMetadata
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.delay
-
 
 class StoreFragment : Fragment() {
 
@@ -32,41 +30,30 @@ class StoreFragment : Fragment() {
         val db = Firebase.firestore;
         val sharedContent = db.collection("shared");
 
-        val recyclerView1 = root.findViewById<RecyclerView>(R.id.recyclerView1);
-        val recyclerView2 = root.findViewById<RecyclerView>(R.id.recyclerView2);
-        val recyclerView3 = root.findViewById<RecyclerView>(R.id.recyclerView3);
-        val recyclerView4 = root.findViewById<RecyclerView>(R.id.recyclerView4);
+        val newModulesRV = root.findViewById<RecyclerView>(R.id.new_modules_recyclerview);
+        val trendModulesRV = root.findViewById<RecyclerView>(R.id.trending_modules_recyclerview);
+        val newProjectsRV = root.findViewById<RecyclerView>(R.id.new_projects_recyclerview);
+        val trendProjectsRV = root.findViewById<RecyclerView>(R.id.trending_projects_recyclerview);
 
-        recyclerView1.layoutManager = LinearLayoutManager(context);
-        recyclerView2.layoutManager = LinearLayoutManager(context);
-        recyclerView3.layoutManager = LinearLayoutManager(context);
-        recyclerView4.layoutManager = LinearLayoutManager(context);
+        newModulesRV.layoutManager    = LinearLayoutManager(context);
+        trendModulesRV.layoutManager  = LinearLayoutManager(context);
+        newProjectsRV.layoutManager   = LinearLayoutManager(context);
+        trendProjectsRV.layoutManager = LinearLayoutManager(context);
 
         sharedContent.get().addOnSuccessListener { document ->
             if (document != null) {
-                // TODO: FIX THIS SHT, ITS NOT WORKING
+                val storeItemList: List<StoreItemMetadata> = document.map { it.toObject() }
 
-                val StoreItemList: List<StoreItemMetadata> = ArrayList();
-
-                for (snapshot in document) {
-                    StoreItemList.plus(snapshot.toObject(StoreItemMetadata::class.java));
-                    Log.d("sus", StoreItemList.toString());
-                }
-
-                recyclerView1.adapter = StoreItemAdapter(StoreItemList);
-                recyclerView2.adapter = StoreItemAdapter(StoreItemList);
-                recyclerView3.adapter = StoreItemAdapter(StoreItemList);
-                recyclerView4.adapter = StoreItemAdapter(StoreItemList);
+                newModulesRV.adapter    = StoreItemAdapter(storeItemList);
+                trendModulesRV.adapter  = StoreItemAdapter(storeItemList);
+                newProjectsRV.adapter   = StoreItemAdapter(storeItemList);
+                trendProjectsRV.adapter = StoreItemAdapter(storeItemList);
             } else {
                 Log.d("DataBase", "No such document");
             }
+
         }.addOnFailureListener { exception -> Log.d("DataBase", "get failed with ", exception); };
 
         return root;
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = StoreFragment();
     }
 }
