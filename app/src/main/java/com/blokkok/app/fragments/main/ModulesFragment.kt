4161c2c8a60734e752408ac449813aba4668c535
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-28 15:45:05 +0700

    feat: implement module importing on ModuleFragment

--- app/src/main/java/com/blokkok/app/fragments/main/ModulesFragment.kt
+++ app/src/main/java/com/blokkok/app/fragments/main/ModulesFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.blokkok.app.R
 import com.blokkok.app.viewmodels.main.ModulesViewModel
+import com.blokkok.modsys.ModuleManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.zip.ZipInputStream
 
 class ModulesFragment : Fragment() {
     val viewModel: ModulesViewModel by viewModels()
@@ -21,11 +25,25 @@ class ModulesFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_modules, container, false)
         val addModuleFab = root.findViewById<FloatingActionButton>(R.id.addModule)
 
+        val importModule = registerForActivityResult(ActivityResultContracts.OpenDocument()) {
+            if (it == null) return@registerForActivityResult
+
+            viewModel.importModule(requireContext().contentResolver, it)
+        }
+
         addModuleFab.setOnClickListener {
-            TODO("Implement add module fab")
+            importModule.launch(arrayOf("application/zip"))
         }
 
         return root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel.toastAction.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
--- app/src/main/java/com/blokkok/app/viewmodels/main/ModulesViewModel.kt
+++ app/src/main/java/com/blokkok/app/viewmodels/main/ModulesViewModel.kt
@@ -1,7 +1,32 @@
 package com.blokkok.app.viewmodels.main
 
+import android.content.ContentResolver
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.blokkok.modsys.ModuleManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.zip.ZipInputStream
 
 class ModulesViewModel : ViewModel() {
 
+    private val toastActionMutable = MutableLiveData<String>()
+    val toastAction: LiveData<String> = toastActionMutable
+
+    fun importModule(contentResolver: ContentResolver, uri: Uri) {
+        val descriptor = contentResolver.openAssetFileDescriptor(uri, "r")!!
+        val inputStream = descriptor.createInputStream()
+
+        viewModelScope.launch(Dispatchers.IO) {
+            ModuleManager.importModule(ZipInputStream(inputStream))
+
+            withContext(Dispatchers.Main) {
+                toastActionMutable.value = "Module has been successfully imported"
+            }
+        }
+    }
 }
