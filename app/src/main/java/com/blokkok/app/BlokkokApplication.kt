6c2f3f3440971ba3fec674626ee09419d73aa774
Iyxan23 <nurihsanalghifari@gmail.com>
2021-07-30 21:40:25 +0700

    feat: Implement exception handler on Application

--- app/src/main/java/com/blokkok/app/BlokkokApplication.kt
+++ app/src/main/java/com/blokkok/app/BlokkokApplication.kt
@@ -1,6 +1,11 @@
 package com.blokkok.app
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Process
 import com.blokkok.app.managers.CommonFilesManager
 import com.blokkok.app.managers.NativeBinariesManager
 import com.blokkok.app.managers.libraries.LibraryManager
@@ -9,8 +14,11 @@ import com.blokkok.app.processors.compilers.ECJCompiler
 import com.blokkok.app.processors.dexers.D8Dexer
 import com.blokkok.app.processors.signers.AndroidApkSigner
 import com.blokkok.modsys.ModuleManager
+import kotlin.system.exitProcess
+
 
 class BlokkokApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
 
@@ -23,5 +31,28 @@ class BlokkokApplication : Application() {
         AndroidApkSigner        .initialize(this)
         LibraryManager          .initialize(this)
         CommonFilesManager      .initialize(this)
+
+        initializeExceptionHandler()
+    }
+
+    private fun initializeExceptionHandler() {
+        Thread.setDefaultUncaughtExceptionHandler { _, ex ->
+            val intent = Intent(applicationContext, DebugActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+                putExtra("error", ex.stackTraceToString())
+            }
+
+            val pendingIntent = PendingIntent.getActivity(applicationContext,
+                0,
+                intent,
+                PendingIntent.FLAG_ONE_SHOT
+            )
+
+            val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            am[AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000] = pendingIntent
+
+            Process.killProcess(Process.myPid())
+            exitProcess(1)
+        }
     }
 }
