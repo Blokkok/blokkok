c927895ad7e7de8f31cba5a32fe2ebce87379b44
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-08 15:05:59 +0700

    feat: add global namespace dump pref in settings

--- app/src/main/java/com/blokkok/app/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/blokkok/app/ui/fragments/SettingsFragment.kt
@@ -3,9 +3,12 @@ package com.blokkok.app.ui.fragments
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.blokkok.app.R
+import com.blokkok.modsys.namespace.NamespaceResolver
 
 class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -29,5 +32,23 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
                 else AppCompatDelegate.MODE_NIGHT_NO
             )
         }
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        return preference?.let { pref ->
+            when (pref.key) {
+                "dump_global_namespace" -> {
+                    Log.d("NamespaceDump", "Global namespace dump:\n" + StringBuilder().apply {
+                        NamespaceResolver.globalNamespace.prettyPrint {
+                            appendLine(it)
+                        }
+                    }.toString())
+
+                    true
+                }
+
+                else -> false
+            }
+        } ?: false
     }
 }
